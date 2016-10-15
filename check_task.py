320b6a397a30af0e757320d05df32bb1707305a0
Gregory Petukhov <lorien@lorien.name>
2016-10-15 17:38:50 +0600

    Implement multi-process processing the task file

--- check_task.py
+++ check_task.py
@@ -2,16 +2,41 @@
 from argparse import ArgumentParser
 import yaml
 from check_plist import check_plist
+from multiprocessing import Process, Queue
+from queue import Empty
+
+
+def worker(taskq):
+    while True:
+        try:
+            task = taskq.get_nowait()
+        except Empty:
+            break
+        else:
+            print('Checking %s' % (task['plist_url']))
+            check_plist(**task)
+
+
+def load_task_queue(task_file):
+    taskq = Queue()
+    for task in yaml.load(open(task_file)):
+        taskq.put(task)
+    return taskq
+
 
 def main():
     parser = ArgumentParser()
     parser.add_argument('task_file')
+    parser.add_argument('-w', '--workers', type=int, default=1)
     opts = parser.parse_args()
-    task_list = yaml.load(open(opts.task_file))
-    for task in task_list:
-        print('Checking %s' % (task['plist_url']))
-        check_plist(**task)
-
+    taskq = load_task_queue(opts.task_file)
+    pool = []
+    for x in range(opts.workers):
+        pr = Process(target=worker, args=[taskq])
+        pr.start()
+        pool.append(pr)
+    for pr in pool:
+        pr.join()
 
 
 if __name__ == '__main__':
