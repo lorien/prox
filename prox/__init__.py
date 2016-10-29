0b6439473544370fd8a47ab951eaf67fd826a978
Gregory Petukhov <lorien@lorien.name>
2016-10-29 17:32:07 +0300

    Bump version: 0.0.13 → 0.0.14

--- prox/__init__.py
+++ prox/__init__.py
@@ -1,6 +1,6 @@
 import os
 
-__version__ = '0.0.13'
+__version__ = '0.0.14'
 
 country_database_file = os.path.join(os.path.dirname(__file__),
                                      'database', 'GeoLite2-Country.mmdb')
