b2d26885cdbac7b49dd2c2f2909126df675a09ed
Gregory Petukhov <lorien@lorien.name>
2016-10-29 17:36:51 +0300

    Bump version: 0.0.15 → 0.0.16

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ ROOT = os.path.dirname(os.path.realpath(__file__))
 
 setup(
     name = 'prox',
-    version = '0.0.15',
+    version = '0.0.16',
     description = 'The tool to check the health of proxy list',
     long_description = open(os.path.join(ROOT, 'README.rst')).read(),
     url = 'http://github.com/lorien/prox',
