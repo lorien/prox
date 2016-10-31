1976a105ffa5ab89e076c63eec07cee33e893976
Maxim Kiselev <MaximKiselev@users.noreply.github.com>
2016-10-31 14:53:47 +0300

    external access for debug

--- prox/api.py
+++ prox/api.py
@@ -49,7 +49,7 @@ def check_find():
 
 
 def debug_server():
-    app.run(host='localhost', port=9000, debug=True, reloader=True)
+    app.run(host='0.0.0.0', port=9000, debug=True, reloader=True)
 
 
 if __name__ == '__main__':
